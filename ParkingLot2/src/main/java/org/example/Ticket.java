abe7a581e0a88c388c397a7c67864079c8ffd299
Bill <bill@fairfieldco.com>
2023-10-26 11:31:55 -0700

    Added the ticket interface

---
+++ ParkingLot2/src/main/java/org/example/Ticket.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public interface Ticket {
+    public int getDays();
+    public int getHours();
+    public int getMinutes();
+}
--- ParkingLot2/src/test/java/unittests.java
+++
@@ -1,7 +0,0 @@
-import org.junit.jupiter.api.Test;
-
-public class unittests {
-
-    @Test
-    public void test() {}
-}
