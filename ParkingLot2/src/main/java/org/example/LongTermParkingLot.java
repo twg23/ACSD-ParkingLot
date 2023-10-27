e241adce6957aaaf1f0feff0efcc58b341fc5904
BackpackTrainer <bill@fairfieldco.com>
2023-10-27 16:01:30 -0400

    master branch is starter code for students.  The solution is on the solution branch

--- ParkingLot2/src/main/java/org/example/LongTermParkingLot.java
+++ ParkingLot2/src/main/java/org/example/LongTermParkingLot.java
@@ -3,19 +3,15 @@ package org.example;
 public class LongTermParkingLot implements ParkingLot{
     @Override
     public int calculateFee(Ticket ticket) {
-        int minutes = ticket.getMinutes();
-        int hours = ticket.getHours();
-        int days= ticket.getDays();
-        int total = 0;
-        if (minutes <= 30 && hours == 0 && days == 0) {
-        } else {
-            if (minutes > 0 ){
-                total+=2;
-            }
-            total+=hours*2;
-            total = Math.min(total, 15);
-            total+=days*15;
-        }
-        return total;
+// ToDo:  Use TDD to Implement this function
+//  You will need to mock the Ticket interface
+//  Business rules for this parking lot are as follows:
+//        Basic fee is $2 per hour or fraction thereof
+//        This means we always round up; ie, 4 hours 2 minutes is billed as 5 hours.
+//        There is no pro-ration
+//        Daily max is $15
+//        On the first day, the first half hour is free
+
+        return 0;
     }
 }
--- ParkingLot2/src/main/java/org/example/PremiumLot.java
+++ ParkingLot2/src/main/java/org/example/PremiumLot.java
@@ -4,22 +4,15 @@ public class PremiumLot implements ParkingLot {
 
     @Override
     public int calculateFee(Ticket ticket) {
-        int days = ticket.getDays();
-        int hours = ticket.getHours();
-        int minutes = ticket.getMinutes();
+// ToDo:  Use TDD to Implement this function
+//  You will need to mock the Ticket interface
+//  Business rules for this parking lot are as follows:
+//        Basic fee is $4 per hour or fraction thereof
+//        This means we always round up; ie, 4 hours 2 minutes is billed as 5 hours.
+//        There is no pro-ration
+//        Daily max is $27
+//        Every 7th day or fraction thereof is free.  (ie, a stay of 13 days and 4 hours will be billed for 12 days
 
-        if (minutes > 0) {
-            hours += 1;
-        }
-
-        int hoursCharge = Math.min(hours * 4, 27);
-
-        if (days % 7 == 6) {
-            hoursCharge = 0;
-        }
-
-        days -= days / 7;
-
-        return (days * 27) + hoursCharge;
+        return 0;
     }
 }
