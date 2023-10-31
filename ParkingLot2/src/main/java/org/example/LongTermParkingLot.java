80f728ccf481ba98f2a63433e5430faa357f8ff6
twg23 <toluadetomiwa@gmail.com>
2023-10-31 09:22:49 +0000

    ACSD-ParkingLot

--- ParkingLot2/src/main/java/org/example/LongTermParkingLot.java
+++ ParkingLot2/src/main/java/org/example/LongTermParkingLot.java
@@ -1,8 +1,108 @@
 package org.example;
 
 public class LongTermParkingLot implements ParkingLot{
-    @Override
-    public int calculateFee(Ticket ticket) {
+
+        @Override
+        public int calculateFee(Ticket ticket) {
+            int dailyRate = 2;
+            int dailyMax = 15;
+
+            int hour = ticket.getHours();
+            int minutes = ticket.getMinutes();
+
+            if (hour == 0 && minutes <= 30) {
+                return 0; // No fee for less than or equal to 30 minutes.
+            }
+
+            if (minutes > 30) {
+                hour++; // Round up to the nearest hour
+            }
+
+            int totalFee = hour * dailyRate;
+
+            if (totalFee > dailyMax) {
+                return dailyMax;
+            }
+
+            return totalFee;
+        }
+    }
+
+
+
+ //   @Override
+//    public int calculateFee(Ticket ticket) {
+//        int dailyRate = 2;
+//        double totalFee;
+//        int dailyMax = 15;
+//        double excludeFirstThirtyMinuteFromFee ;
+//        double remainingMinutes;
+//
+//
+//        int hour = ticket.getHours();
+//        double minutes = ticket.getMinutes();
+//
+////        if( minutes % 60 != 0) {
+////            double remainingMinutes = minutes % 60;
+////         if(remainingMinutes >0){
+////               hour++;
+////              totalFee = hour *dailyRate;
+////              if( totalFee > dailyMax){
+////                  return dailyMax;
+////              }
+////           }
+////
+////       }
+//
+//
+////        if (hour ==1 ){
+////           totalFee= dailyRate * hour;
+////
+////            return totalFee;
+////        }
+//
+//
+//        if (minutes > 30) {
+//            excludeFirstThirtyMinuteFromFee = (minutes - 30);
+//            if (excludeFirstThirtyMinuteFromFee % 60 != 0) {
+//                 remainingMinutes = excludeFirstThirtyMinuteFromFee % 60;
+//                if (remainingMinutes > 0) {
+//                   hour= hour++;
+//                    totalFee = hour * dailyRate;
+//                    if (totalFee > dailyMax) {
+//                    return dailyMax;
+//                    }
+//                    else {
+//                    return (int) totalFee;
+//                    }
+//                }
+//                else {
+//           totalFee = (remainingMinutes % 60) *dailyRate;
+//            return (int) totalFee;
+//        }
+//            }
+//        }
+//        return  totalFee;
+//    }
+//}
+
+
+
+//
+////        else{
+////            return 0;
+////        }
+////    return 0;
+//    return }
+
+
+
+
+
+
+
+
+
 // ToDo:  Use TDD to Implement this function
 //  You will need to mock the Ticket interface
 //  Business rules for this parking lot are as follows:
@@ -12,6 +112,7 @@ public class LongTermParkingLot implements ParkingLot{
 //        Daily max is $15
 //        On the first day, the first half hour is free
 
-        return 0;
-    }
-}
+
+
+
+
--- ParkingLot2/src/test/java/ParkingLotRunner.java
+++ ParkingLot2/src/test/java/ParkingLotRunner.java
@@ -1,7 +1,77 @@
+import org.example.LongTermParkingLot;
+import org.example.PremiumLot;
+import org.example.Ticket;
+import org.example.TicketImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
+import org.mockito.Mock;
 
-@Suite
-@SelectClasspathResource("/features")
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import org.junit.Assert;
+
+import static org.mockito.Mockito.when;
+//@Suite
+//@SelectClasspathResource("/features")
+
+
 public class ParkingLotRunner {
+
+    private TicketImpl ticket ;
+    private LongTermParkingLot longTermParkingLot;
+    private PremiumLot premiumLot;
+
+    @BeforeEach
+    void setUp(){
+        longTermParkingLot = new LongTermParkingLot();
+        ticket = new TicketImpl();
+        premiumLot =new PremiumLot();
+    }
+
+    @Test
+    public void shouldCalculateFeeForTheHour(){
+        ticket.setEntryTime("2023-10-30 14:00");
+        ticket.setExitTime("2023-10-30 15:00");
+        int fee =  longTermParkingLot.calculateFee(ticket);
+        Assert.assertEquals( 2, fee);
+    }
+
+    @Test
+    public void  shouldRoundUpMinutesToTheNearestHour(){
+        ticket.setEntryTime("2023-10-30 14:00");
+        ticket.setExitTime("2023-10-30 16:00");
+         int fee = longTermParkingLot.calculateFee(ticket);
+         Assert.assertEquals(4, fee);
+    }
+    @Test
+    public void shouldTotalFeeNotExceedDailyMax(){
+        ticket.setEntryTime("2023-10-30 09:00");
+        ticket.setExitTime("2023-10-30 21:30");
+        int fee = longTermParkingLot.calculateFee(ticket);
+        Assert.assertEquals(15, fee);
+
+    }
+
+    @Test
+    public void shouldAllowFirstHourFree(){
+        ticket.setEntryTime("2023-10-30 09:00");
+        ticket.setExitTime("2023-10-30 10:30");
+        int fee = longTermParkingLot.calculateFee(ticket);
+        Assert.assertEquals(2, fee);
+
+    }
+    @Test
+    public void shouldCalculateFeeForTheHourOnPL(){
+        ticket.setEntryTime("2023-10-30 14:00");
+        ticket.setExitTime("2023-10-30 15:00");
+        int fee =  longTermParkingLot.calculateFee(ticket);
+        Assert.assertEquals( 4, fee);
+    }
+
+
+
+
 }
